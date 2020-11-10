99e49dedae702c1fcfceab323e1a2f87c7f7fdf2
Jennifer Lucas <36136983+jclucas@users.noreply.github.com>
2020-11-09 19:26:52 -0500

    generate facial landmark points

---
+++ landmarks.py
@@ -0,0 +1,63 @@
+import numpy as np
+import dlib
+import cv2
+
+def detect_face(image):
+
+    # preprocessing
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # get face detector
+    detector = dlib.get_frontal_face_detector()
+
+    # detect faces
+    rects = detector(image, 1)
+
+    if (len(rects) < 1):
+        print('No face detected')
+        exit()
+
+    # just grab the first face
+    return rects[0]
+
+
+def generate_landmarks(image, rect):
+
+    # preprocessing
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # initialize face landmark predictor
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+    
+    # get predicted landmarks
+    landmarks = predictor(image, rect)
+
+    # read landmark object into np array of xy coordinates
+    coords = np.zeros((68, 2), dtype='int')
+
+    for i in range(0, 68):
+        coords[i] = (landmarks.part(i).x, landmarks.part(i).y)
+
+    return coords
+
+
+def visualize_landmarks(image_path, output_path=None):
+
+    # load image from disk
+    image = cv2.imread(image_path)
+
+    # detect face and landmarks
+    rect = detect_face(image)
+    shape = generate_landmarks(image, rect)
+
+    # add markers to image
+    for (x, y) in shape:
+            cv2.circle(image, (x, y), 3, (0, 0, 255), -1)
+
+    # save to disk
+    if not (output_path is None):
+        cv2.imwrite(output_path, image)
+    
+    # display
+    cv2.imshow("Output", image)
+    cv2.waitKey(0)
